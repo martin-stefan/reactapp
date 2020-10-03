cf4059da694a2ed3495fc8cfa4affe6e106af607
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2020-10-03 12:53:53 -0400

    general setup for post

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.scss';
+
+import Post from './components/Post';
 
 function App() {
   return (
     <div className="App">
-      hey
+      <Post />
     </div>
   );
 }
