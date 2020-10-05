e992defa34a110764b2ed66f695deddf1acd1017
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2020-10-05 10:05:08 -0400

    added routing

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import Hero from './containers/Hero';
 import Post from './components/Post';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import User from './containers/User';
 
 function App() {
   return (
-    <div className="App">
-      <Hero />
-      {/* <Post /> */}
-    </div>
+    <Router>
+      <div className="App">
+
+        <Switch>
+          <Route path="/posts">
+            <Post />
+          </Route>
+
+          <Route path="/user">
+            <User />
+          </Route>
+
+          <Route path="/">
+            <Hero />
+          </Route>
+
+
+        </Switch>
+      
+        
+      </div>
+    </Router>
   );
 }
 
