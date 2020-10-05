a51c0c18629cde84964e336995e76dc4d9d17dda
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2020-10-04 23:24:45 -0400

    working on hero section

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import Hero from './containers/Hero';
 import Post from './components/Post';
 
 function App() {
   return (
     <div className="App">
-      <Post />
+      <Hero />
+      {/* <Post /> */}
     </div>
   );
 }
