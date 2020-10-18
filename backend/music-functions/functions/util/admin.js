1d1b1250b1942f482b7b80fcd9dd3525975ab973
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2020-10-17 23:41:37 -0400

    including admin file

---
+++ backend/music-functions/functions/util/admin.js
@@ -0,0 +1,10 @@
+const admin = require("firebase-admin");
+
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+});
+
+
+const db = admin.firestore();
+
+module.exports = { admin, db };
